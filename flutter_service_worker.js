ea0124341abe0aca0a373a566c468989e31c67bf
hasundue <hasundue@users.noreply.github.com>
2021-12-10 00:14:48 +0000

    deploy: f6de18005a6343c75b5e1e9aff06c9d193481d99

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,24 +3,24 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "canvaskit/canvaskit.js": "43fa9e17039a625450b6aba93baf521e",
-"canvaskit/profiling/canvaskit.js": "f3bfccc993a1e0bfdd3440af60d99df4",
-"canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
-"canvaskit/canvaskit.wasm": "04ed3c745ff1dee16504be01f9623498",
+  "canvaskit/canvaskit.js": "62b9906717d7215a6ff4cc24efbd1b5c",
+"canvaskit/profiling/canvaskit.js": "3783918f48ef691e230156c251169480",
+"canvaskit/profiling/canvaskit.wasm": "6d1b0fc1ec88c3110db88caa3393c580",
+"canvaskit/canvaskit.wasm": "b179ba02b7a9f61ebc108f82c5a1ecdb",
 "version.json": "c516e31f8914df52f042ba3b1d26fde0",
 "manifest.json": "84085d51e8228ee397bd1db441086d38",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"main.dart.js": "275110242802bc100e957e74d6b7063d",
+"main.dart.js": "b8b02471cd6de915ddbac572ff3f5468",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "d8d85f0a747968a118834bd3adbc32a8",
-"/": "d8d85f0a747968a118834bd3adbc32a8",
+"index.html": "5a1239005e7306220ee629effe186ae7",
+"/": "5a1239005e7306220ee629effe186ae7",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "0e654f7bfe38c1ee61668c76b5ff5de6",
+"assets/NOTICES": "671fb14bcaf0dbf74eb3951a01436ac0",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e"
 };
 
